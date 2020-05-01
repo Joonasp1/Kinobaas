06ae2d142db3e216ca60f3258346e141950a2d87
Joonasp1 <56842740+Joonasp1@users.noreply.github.com>
2020-05-01 16:26:04 +0300

    Tabelid lisatud

---
+++ Andmebaas/tabelid.sql
@@ -0,0 +1,50 @@
+CREATE TABLE Toidud(
+ID integer not null default autoincrement primary key,
+Nimi varchar(100) not null,
+Hind float not null);
+
+CREATE TABLE Saalid(
+ID integer not null default autoincrement primary key,
+Number integer not null,
+Istmete_arv integer not null);
+
+CREATE TABLE Filmid(
+ID integer not null default autoincrement primary key,
+Nimi varchar(200) not null,
+Aasta integer not null,
+Režisöör varchar(100),
+Pileti_baashind float not null);
+
+CREATE TABLE Külastajad(
+ID integer not null default autoincrement primary key,
+Seanss integer not null);
+
+CREATE TABLE Tellimus(
+ID integer not null default autoincrement primary key,
+Toit integer not null,
+Tellija integer not null);
+
+CREATE TABLE Kinosaalid(
+ID integer not null default autoincrement primary key,
+Kino integer not null,
+Saal integer not null);
+
+CREATE TABLE Kinod(
+ID integer not null default autoincrement primary key,
+Nimi varchar(150) not null,
+Päev datetime not null,
+Sissetulek float not null);
+
+CREATE TABLE Seansid(
+ID integer not null default autoincrement primary key,
+Hind float not null,
+Aeg datetime not null,
+KolmD BIT not null, //1 - on 3D 2 - ei ole 3D
+Saal integer not null,
+Film integer not null,
+Külastajate_arv integer not null);
+
+CREATE TABLE Kinoseansid(
+ID integer not null default autoincrement primary key,
+Kino integer not null,
+Seanss integer not null);
